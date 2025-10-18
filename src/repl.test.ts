c8a59e56f85435fef22914a00521e9662b4727ec
mmert9008 <michael.mertens.jr@gmail.com>
2025-10-18 14:58:10 -0500

    initial project setup with dependencies added, typescript configured, and an initial module and test suite

---
+++ src/repl.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from "vitest";
+import { cleanInput } from "./repl.ts";
+
+describe.each([
+  {
+    input: "  hello  world  ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "Charmander Bulbasaur PIKACHU",
+    expected: ["charmander", "bulbasaur", "pikachu"],
+  },
+  {
+    input: "single",
+    expected: ["single"],
+  },
+  {
+    input: "  MULTIPLE   SPACES   BETWEEN  ",
+    expected: ["multiple", "spaces", "between"],
+  },
+  {
+    input: "",
+    expected: [""],
+  },
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    const actual = cleanInput(input);
+
+    expect(actual).toHaveLength(expected.length);
+
+    for (const i in expected) {
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});
